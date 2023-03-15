2a90260265b71b517c91bf8575e6725666cea83e
fabianchouniminuto <fabianchouniminuto@gmail.com>
2023-03-14 23:55:21 -0500

    first commit

---
+++ app/src/main/java/com/android/jgg/materialdesign/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.jgg.materialdesign;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
